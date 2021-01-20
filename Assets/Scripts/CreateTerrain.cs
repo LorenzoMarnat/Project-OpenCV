0ba294aba0a89546847bc423dda2a6eb68c8f2a8
LorenzoMarnat <lmarnat14@gmail.com>
2021-01-20 09:49:15 +0100

    Init emguCV

---
+++ Assets/Scripts/CreateTerrain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+public class CreateTerrain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Image<Gray,Byte> image = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
